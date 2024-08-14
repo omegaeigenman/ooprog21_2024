c54afc7427add1487626f5bb68ea618335f5b108
Omega <150210789+omegaeigenman@users.noreply.github.com>
2024-08-15 00:00:09 +0800

    Add files via upload

---
+++ Chapter1/MovieQuoteInfo.java
@@ -0,0 +1,13 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+      String quote = "Yesterday is history, Tomorrow is a mystery, but Today is a gift. That is why it is called the present.";
+      String movie = "Kung Fu Panda";
+      String character = "Oogway";
+      int year = 2008;
+  
+      System.out.println("Quote: " + quote);
+      System.out.println("Movie: " + movie);
+      System.out.println("Character: " + character);
+      System.out.println("Year: " + year);
+    }
+  }
