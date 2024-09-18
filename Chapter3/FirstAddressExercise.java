164c48daa819371e03abe8fad3afc8cc25255c59
Omega <150210789+omegaeigenman@users.noreply.github.com>
2024-09-18 08:58:02 +0800

    Update FirstAddressExercise.java

--- Chapter3/FirstAddressExercise.java
+++ Chapter3/FirstAddressExercise.java
@@ -1,6 +1,6 @@
 public class FirstAddressExercise {
    public static void displayAdress(){
-       System.out.println("Huawei Technologies Co., Ltd.")
+       System.out.println("Huawei Technologies Co., Ltd.");
        System.out.println("Huawei Headquarters");
        System.out.println("Bantian Street, Longgang District");
        System.out.println("Shenzhen, Guangdong, China");
